ac707520be7bb1fbad5a7080ed5edba40a2e8dfb
liza140 <liza140@mail.ru>
2022-05-19 22:19:16 +0300

    двумерный массив размером m×n, заполненный целыми числами

---
+++ Array1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+// Создание массива
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+// Заполнение массива
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+// Печать массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int M = 3;
+int N = 4;
+int[,] Array = CreateArray(M, N);
+FillArray(Array);
+PrintArray(Array);
