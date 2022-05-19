e91d1d38cf8d3ba2b707a348a140bfb7467042cd
liza140 <liza140@mail.ru>
2022-05-19 22:27:34 +0300

    двумерный массив размером m×n, заполненный вещественными числами

---
+++ Array2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Показать двумерный массив размером m×n, заполненный вещественными числами.
+
+// Создание массива
+double[,] CreateArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+// Заполнение массива
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(-9, 10) + new Random().NextDouble(), 3);
+        }
+    }
+}
+
+// Печать массива
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int M = 3;
+int N = 4;
+double[,] Array = CreateArray(M, N);
+FillArray(Array);
+PrintArray(Array);
+
