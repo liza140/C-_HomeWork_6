425e76e937bad9c8fe11590b7577bc6738e5dc58
liza140 <liza140@mail.ru>
2022-05-19 22:37:17 +0300

    Замена четных элементов двумерного массива на противоположные

---
+++ Array3/Program.cs
@@ -0,0 +1,62 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+// Создание массива
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+// Заполнение массива
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-9, 10);
+        }
+    }
+}
+
+// Печать массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Замена четных элементов на противоположные
+
+void ChangePositiveElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0) array[i, j] = -array[i, j];
+        }
+    }
+}
+
+int M = 3;
+int N = 4;
+int[,] Array = CreateArray(M, N);
+FillArray(Array);
+PrintArray(Array);
+Console.WriteLine();
+ChangePositiveElements(Array);
+PrintArray(Array);
