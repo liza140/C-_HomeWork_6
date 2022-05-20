8d7acd640b856bd5672173612a1ec3b8d88ca4a7
liza140 <liza140@mail.ru>
2022-05-20 23:22:55 +0300

    Поиск заданного числа в массиве

---
+++ Array6/Program.cs
@@ -0,0 +1,89 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет.
+
+// Создание массива
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+// Заполнение массива
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-9, 10);
+        }
+    }
+}
+
+// Печать массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Поиск элемента массива
+bool FindElements(int[,] array, int num)
+{
+    bool findElements = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num) findElements = true;
+        }
+    }
+    return findElements;
+}
+
+(int, int) FindIndex(int[,] array, int num)
+{
+    int indI = 0;
+    int indJ = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+            {
+                indI = i;
+                indJ = j;
+            }
+        }
+    }
+    return(indI, indJ);
+}
+
+int M = 3;
+int N = 4;
+int Num = 8;
+int[,] Array = CreateArray(M, N);
+FillArray(Array);
+PrintArray(Array);
+Console.WriteLine();
+bool logik = FindElements(Array, Num);
+if (logik == true)
+{
+    Console.Write($"Позиция заданного пользователем числа ");
+    Console.WriteLine(FindIndex(Array, Num));
+}
+else Console.WriteLine("Заданное пользователем число отсутсвует в массиве");
+
+
