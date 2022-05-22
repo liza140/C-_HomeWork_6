3d40819f94e0a7b660628c0518b932338bfdaf74
liza140 <liza140@mail.ru>
2022-05-22 10:55:54 +0300

    Поиск среднего арифметического каждого из столбцов двумерного массива_1

--- Array8/Program.cs
+++ Array8/Program.cs
@@ -4,13 +4,6 @@
 int[,] CreateArray(int m, int n)
 {
     int[,] array = new int[m, n];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = i + j;
-        }
-    }
     return array;
 }
 
