1b18957d9d2b6d6eac874d3cdd4c28eef2a97980
liza140 <liza140@mail.ru>
2022-05-22 10:40:42 +0300

    Двумерный массив вещественных чисел

--- Array2/Program.cs
+++ Array2/Program.cs
@@ -4,13 +4,6 @@
 double[,] CreateArray(int m, int n)
 {
     double[,] array = new double[m, n];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = i + j;
-        }
-    }
     return array;
 }
 
